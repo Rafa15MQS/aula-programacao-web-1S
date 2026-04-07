f80bc7175b017a45780b30964e5caa241fe63001
Rafa15MQS <Rafaela.marques97@hotmail.com>
2026-04-07 12:23:53 -0300

    .

---
+++ aula08/js/script.js
@@ -0,0 +1,19 @@
+function testeExternal(){
+    alert('Olá Mundo - Teste External')
+}
+
+function testeConfirm(){
+    let resp = confirm('Você aceita essa ação')
+
+    console.log(resp)
+}
+
+function testePrompt(){
+    let resp = prompt('Qual ´pe o seu nome?')
+
+    console.log(resp)
+}
+
+
+
+document.getElementById("meuID"). innerText="Novo Texto"
